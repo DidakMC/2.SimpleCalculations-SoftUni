ec5db192a48439e8d2472b0e5da3747a8a95e743
Diana <tonidid2003@gmail.com>
2018-01-28 11:54:28 +0200

    Update Program.cs

--- 1000 days on Earth/Program.cs
+++ 1000 days on Earth/Program.cs
@@ -10,6 +10,32 @@ namespace _1000_days_on_Earth
     {
         static void Main(string[] args)
         {
+             
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace ThousandDays
+{
+    class ThousandDays
+    {
+        static void Main(string[] args)
+        {
+            string dateString = Console.ReadLine();
+            string format= "dd-MM-yyyy";
+            DateTime result=DateTime.ParseExact(dateString, format, CultureInfo.InvariantCulture);
+            CultureInfo provider = CultureInfo.InvariantCulture;
+            DateTime output = Convert.ToDateTime(result).AddDays(999);
+
+            Console.WriteLine(output.ToString("dd-MM-yyyy"));
+
+        }
+    }
+}
         }
     }
 }
